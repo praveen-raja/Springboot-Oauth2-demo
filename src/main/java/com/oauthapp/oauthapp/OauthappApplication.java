395d16f2b831d515f05a210c8f8a877afd89b8cd
praveen <praveen2099636@gmail.com>
2022-09-01 11:58:51 +0530

    Initial commit

---
+++ src/main/java/com/oauthapp/oauthapp/OauthappApplication.java
@@ -0,0 +1,13 @@
+package com.oauthapp.oauthapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oauthapp/oauthapp/OauthappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oauthapp.oauthapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
